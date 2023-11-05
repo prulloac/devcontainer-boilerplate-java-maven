a6238d253a6e87cfb0d9a1146829b4827abffae5
Pablo Ulloa <pablo.ulloac@usach.cl>
2023-11-05 23:35:55 +0000

    initial sample project

---
+++ spring-sample-crud-api/src/main/java/dev/prulloac/crudrestapidemo/CrudRestApiDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.prulloac.crudrestapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudRestApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudRestApiDemoApplication.class, args);
+	}
+
+}
---
+++ spring-sample-crud-api/src/test/java/dev/prulloac/crudrestapidemo/CrudRestApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.prulloac.crudrestapidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudRestApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
